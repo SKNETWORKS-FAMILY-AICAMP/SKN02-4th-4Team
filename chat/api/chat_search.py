eb73c5a9f8aaaaf278ec6f8b2002bfaca3f8483c
SnorLaXD98 <105333311+SnorLaXD98@users.noreply.github.com>
2024-09-13 17:22:56 +0900

    Update chat_search.py
    주석 제거

--- chat/api/chat_search.py
+++ chat/api/chat_search.py
@@ -185,64 +185,8 @@ def custom_chain(question : str):
         result = qa({"question":question})
         
         return result
-    # if retriever in retrievers:
-    #     print(response)
-    #     # # 리트리버를 통해 검색 결과 가져옴
-    #     # retrieved_data = retriever.invoke(response.query)
-       
-    #     # Conversational Retrieval Chain 생성
-    #     qa = ConversationalRetrievalChain.from_llm(
-    #         llm=llm,
-    #         retriever=retriever,
-    #         memory=memory,
-    #         combine_docs_chain_kwargs={"prompt": promptse}
-    #     )
-    #     # 답변 반환
-    #     result = qa({"question":question})
-        
-    #     return result
+  
     
-    # else: 
-            
-    #         promptse = promptse = ChatPromptTemplate(
-    #         messages=[
-    #             SystemMessagePromptTemplate.from_template(
-    #                 """
-    #                 사용자가 자동차 설명에 대한 질문이 아닌 다른 질문을 하고있으니 목적에 맞는 질문을 해달라고 요청하십시오.
-
-    #                 너는 친절하고 기억력이 좋은 챗봇이며, 사용자의 질문에 정확하고 일관성 있게 답변할 수 있는 능력을 갖추고 있습니다. 다음 지침에 따라 질문에 답하십시오:
-
-    #                 정보 누락 시 대처:
-    #                     - 만약 필요한 정보를 찾지 못하거나 검색 결과가 충분하지 않다면, 사용자에게 해당 정보를 제공할 수 없음을 알리고, 구체적인 추가 정보를 요청하십시오.
-
-    #                 답을 모를 경우:
-    #                     - "모릅니다"라고만 답하고, 답을 지어내지 마십시오. 모르는 것은 반드시 "모릅니다"라고 답해야 합니다.
-
-    #                 현재 대화내용 : {chat_history}
-    #                 이전 대화의 맥락을 참고하여 질문에 정확하고 일관성 있는 답변을 제공하십시오.
-    #                 이전 대화가 없다면 다시 질문을 해달라고 요청하십시오.
-
-    #                 다음은 실제로 사용자에게 보여질 답변입니다:
-    #                 {context}를 주로 활용해서 대답하십시오.
-    #                 """
-    #             ),
-    #             HumanMessagePromptTemplate.from_template("{question}"),
-    #         ]
-    #     )
-
-    #         qa = ConversationalRetrievalChain.from_llm(
-    #             llm=llm,
-    #             retriever=retriever,
-    #             memory=memory,
-    #             combine_docs_chain_kwargs={"prompt": promptse}
-    #         )
-    #         # 답변 반환
-    #         result = qa({"question":question})
-        
-    #         return result
-    # return ""
-    
-
 
 # custom_chain을 호출하는 함수
 def get_chatbot_response(question: str):
